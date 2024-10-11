9030dde54a0897fc9688c3cd02c746bad900b497
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-10-11 06:33:18 +0300

    Moved logic to create new blog

--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -1,21 +1,72 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
+import { Notification } from "../App";
 
-const BlogForm = ({ onSubmit, handleChange, value }) => {
+const BlogForm = ({ createBlog }) => {
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const [url, setUrl] = useState('');
+    const [notificationMessage, setNotificationMessage] = useState(null);
+    const [notificationType, setNotificationType] = useState(null);
+
+    const handleBlogSubmit = async (event) => {
+        event.preventDefault();
+    
+        try {
+            const blogObject = { title, author, url };
+            createBlog(blogObject)
+        
+            setNotificationMessage(
+                `A new blog ${title} by ${author} added`
+            );
+            setNotificationType('success');
+            setTimeout(() => {
+                setNotificationMessage(null);
+                setNotificationType(null);
+            }, 5000);
+        
+            setTitle('');
+            setAuthor('');
+            setUrl('');
+        } catch (error) {
+            console.error("Error creating new blog", error.message);
+            setNotificationMessage(`An error occurred when creating "${title}" blog`);
+            setNotificationType('error');
+            setTimeout(() => {
+                setNotificationMessage(null);
+                setNotificationType(null);
+            }, 5000);
+        }
+    }
     return (
         <div>
+            <Notification message={notificationMessage} type={notificationType} />
+
             <h2>Create New Blog</h2>
-            <form onSubmit={onSubmit}>
-                <input value={value} onChange={handleChange} />
-                <button type="submit">Save</button>
+            <form onSubmit={handleBlogSubmit}>
+            <div>
+                <label htmlFor="title">Title</label>
+                <input type="text" name="title" id='title' value={title} onChange={({ target }) => setTitle(target.value)} placeholder='title' />
+            </div>
+
+            <div>
+                <label htmlFor="author">Author</label>
+                <input type="text" name="author" id="author" value={author} onChange={({ target }) => setAuthor(target.value)} placeholder='author' />
+            </div>
+
+            <div>
+                <label htmlFor="url">Url</label>
+                <input type="text" name="url" id="url" value={url} onChange={({ target }) => setUrl(target.value)} placeholder='url' />
+            </div>
+
+            <button type="submit">Save</button>
             </form>
         </div>
     )
 };
 
 BlogForm.propTypes = {
-    onSubmit: PropTypes.func,
-    handleChange: PropTypes.func,
-    value: PropTypes.string
+    createBlog: PropTypes.func
 };
 
 export default BlogForm;
