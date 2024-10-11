e546d119a82c26db866712ad06beba3dae35eaa3
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-10-11 06:47:25 +0300

    proptypes

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,14 @@
+import PropTypes from "prop-types";
+
 const Blog = ({ blog }) => (
   <div>
     {blog.title} {blog.author}
+    <button>View</button>
   </div>  
-)
+);
+
+Blog.propTypes = {
+  blog: PropTypes.object
+}
 
 export default Blog
