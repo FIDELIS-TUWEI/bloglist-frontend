a52634191fb25bef65cb7dd5eae10260c589ef19
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-10-11 07:18:06 +0300

    render blog by id with view/hide btn and like btn

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,14 +1,44 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-    <button>View</button>
-  </div>  
-);
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  };
+
+  return (
+    <div style={blogStyle}>
+      <p>{blog.title} {blog.author} </p>
+      <button onClick={toggleVisibility}>
+        {visible ? 'hide' : 'view'}
+      </button>
+
+      {visible && (
+        <div>
+          <p>Title: {blog.title}</p>
+          <p>Url: {blog.url}</p>
+          <p>Likes: {blog.likes}
+            <button>like</button>
+          </p>
+          <p>Author: {blog.author}</p>
+        </div>
+      )}
+    </div>  
+  )
+};
 
 Blog.propTypes = {
-  blog: PropTypes.object
+  blog: PropTypes.object.isRequired
 }
 
 export default Blog
