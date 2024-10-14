85508b85079fc15408d5d9ec4f19f0519fb21efb
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-10-14 07:14:32 +0300

    linting rules

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,9 +1,9 @@
-import axios from "axios";
-const baseUrl = '/api/v1/auth/login';
+import axios from 'axios'
+const baseUrl = '/api/v1/auth/login'
 
 const login = async credentials => {
-    const response = await axios.post(baseUrl, credentials);
-    return response.data;
-};
+    const response = await axios.post(baseUrl, credentials)
+    return response.data
+}
 
 export default { login }
